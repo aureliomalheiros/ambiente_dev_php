7c7b3626f069e62e7d30b477f94d9ddfa4111ac0
Aurelio Malheiros <aureliomalheiros@gmail.com>
2020-04-15 23:05:50 -0300

    Correções do arquivo

--- ambiente_dev_php.sh
+++ ambiente_dev_php.sh
@@ -6,7 +6,7 @@ sudo apt update
 sudo apt upgrade -y
 
 function RunningApache2 () {
-	if [ pgrep apache2 ] 
+	if [ !$(pgrep apache2 | wc -l) -ne 0] 
 	then
 		sudo systemctl enable apache2
 		sudo systemctl start apache2
@@ -14,7 +14,7 @@ function RunningApache2 () {
 }
 
 function RunningMariaDB () {
-	if [ pgrep mariadb ]
+	if [ ! $(ps aux | grep "mariadb" | wc -l) ]
 	then
 		sudo systemctl enable mariadb
 		sudo systemctl start mariadb
@@ -22,12 +22,16 @@ function RunningMariaDB () {
 }
 
 function RunningPHP () {
-	if [ pgrep php7.3 2 ]
+	if [ ! $(ps aux | grep "php" | wc -l) ]
 	then
 		sudo a2enmod php7.3
 		sudo systemctl restart apache2
-		sudo echo -e "<h1>Configured PHP environment</h1>\n$(hostname -f)" > /var/www/html/info.php
+		sudo chmod o+w /var/www/html
 	fi
+}
+function GenerateFile (){
+	sudo echo -e "<h1>Configured PHP environment</h1>\nHostname:$(hostname -f)\nDate:$(date -R)" > /var/www/html/info.php
+	echo "http://localhost/info.php"
 }
 
 if [ ! $(dpkg --get-selections | grep apache2 ) 2> /dev/null ]
@@ -53,6 +57,8 @@ if [ ! $(dpkg --get-selections | grep php) 2> /dev/null ]
 then
 	sudo apt install -y php php-{mysql,common,cli,json,opcache,readline}
 	RunningPHP
+	GenerateFile
 else
 	echo "php7 package is already installed"
+	GenerateFile
 fi
